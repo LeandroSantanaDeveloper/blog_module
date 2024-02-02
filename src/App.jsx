b68a5489249c7c4a055e4a52a42ec4add0d5387c
leandro.santana <leandro.santana@mprj.mp.br>
2024-02-01 22:34:31 -0300

    feat: agora vai

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar'
 
 import './app.css'
+import Home from './pages/Home'
+import NewPost from './pages/NewPost'
+import Post from './pages/post'
+import EditPost from './pages/EditPost'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
       <div className="container">
-        <Outlet />
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/new' element={<NewPost/>}/>
+          <Route path= "/posts/:id" element={<Post/>}/>
+          <Route path= "/posts/:id/edit" element={<EditPost/>}/>
+        </Routes>
+      </BrowserRouter>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,37 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import NewPost from './pages/NewPost.jsx'
-import EditPost from './pages/EditPost.jsx'
-
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/new",
-        element: <NewPost />
-      },
-      // {
-      //   path: "/posts/:id",
-      //   element: <Post/>
-      // },
-      {
-        path: "/posts/:id/edit",
-        element: <EditPost/>
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </React.StrictMode>
 )
