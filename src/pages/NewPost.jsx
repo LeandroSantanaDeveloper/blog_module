d6a64b6421d58f65e20731f2078f9744df38d79f
leandro.santana <leandro.santana@mprj.mp.br>
2024-02-01 23:15:21 -0300

    feat: add style new post

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import usePost from '../hooks/usePost';
-
+import '../styles/new-post.css'
 const NewPost = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
